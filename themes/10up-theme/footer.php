f53ce3d1d0eea4b054fdc11256bbfc7e8f7d3220
Terry Sutton <saltcod@gmail.com>
2021-08-17 09:36:47 -0230

    Finish new theme

--- themes/10up-theme/footer.php
+++ themes/10up-theme/footer.php
@@ -11,6 +11,7 @@
 <div class='site-footer'>
 	<div class="container">
 		<div class="site-footer__wrapper">
+			<hr class="hr--short">
 			<p class="site-footer__site-name">Visit #10up-gutenberg in Slack for help</p>
 		</div>
 	</div>
