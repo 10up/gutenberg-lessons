852fef9cab78d4fb5e4854de31a2b487c284805a
lcnogueira <luizcns18@gmail.com>
2024-02-02 15:41:26 -0300

    fix typo

--- themes/tenup-theme/includes/blocks/inner-blocks-one-starter/edit.js
+++ themes/tenup-theme/includes/blocks/inner-blocks-one-starter/edit.js
@@ -10,7 +10,7 @@ const BockEdit = () => {
 
 	return (
 		<section {...blockProps}>
-			<div className="wp-block-gutenberg-lessons-inner-blocks-one-starter}__image">
+			<div className="wp-block-gutenberg-lessons-inner-blocks-one-starter__image">
 				<img
 					src="https://images.unsplash.com/photo-1631005258849-aee1ccce98f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&h=600&q=80"
 					height="600"
@@ -18,7 +18,7 @@ const BockEdit = () => {
 					alt="Sunset over a Mountainrange"
 				/>
 			</div>
-			<div className="wp-block-gutenberg-lessons-inner-blocks-one-starter}__content">
+			<div className="wp-block-gutenberg-lessons-inner-blocks-one-starter__content">
 				{/* The InnerBlocks should get inserted here */}
 			</div>
 		</section>
